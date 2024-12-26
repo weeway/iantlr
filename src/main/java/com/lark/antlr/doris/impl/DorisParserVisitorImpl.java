9a68dacc367e0da79fb707d85da91857a65fcf51
weeway <geverway@gmail.com>
2024-12-26 14:11:22 +0800

    增加alter和create表名解析

--- src/main/java/com/lark/antlr/doris/impl/DorisParserVisitorImpl.java
+++ src/main/java/com/lark/antlr/doris/impl/DorisParserVisitorImpl.java
@@ -20,7 +20,7 @@ public class DorisParserVisitorImpl extends DorisParserBaseVisitor<String> {
 
     @Override
     public String visitTableName(DorisParser.TableNameContext ctx) {
-        if (hasAncestor(ctx, DorisParser.RULE_fromClause)){
+        if (hasAncestorByRuleIndex(ctx, DorisParser.RULE_fromClause)){
             sourceTables.add(ctx.getText());
             logger.info("visitTableName, source table: "+ctx.getText());
         }else {
@@ -30,21 +30,53 @@ public class DorisParserVisitorImpl extends DorisParserBaseVisitor<String> {
         return visitChildren(ctx);
     }
 
-    @Override public String visitFromClause(DorisParser.FromClauseContext ctx) {
-        // logger.info("visitFromClause");
+    @Override
+    public String visitMultipartIdentifier(DorisParser.MultipartIdentifierContext ctx) {
+        // create/alter 表名解析
+        // 方法1：通过确定父语句来确定是alter还是create
+        if (hasAncestorByClassName(ctx, "DorisParser$AlterTableContext")
+                || hasAncestorByClassName(ctx, "DorisParser$CreateTableContext")){
+            targetTables.add(ctx.getText());
+        }
         return visitChildren(ctx);
     }
 
     @Override public String visitInsertTable(DorisParser.InsertTableContext ctx) {
-
+        // insetTable表名解析
         targetTables.add(ctx.tableName.getText());
         return visitChildren(ctx);
     }
 
-    private Boolean hasAncestor(ParserRuleContext ctx, int ancestorIdx) {
+    @Override public String visitCreateTable(DorisParser.CreateTableContext ctx){
+        // createTable表名解析
+        // 方式2: 直接通过访问createTable子句别名访问
+        logger.info("visitCreateTable: {}", ctx.multipartIdentifier().getText());
+        return visitChildren(ctx);
+    }
+
+    @Override public String visitAlterTable(DorisParser.AlterTableContext ctx){
+        // alterTable表名解析
+        // 方式2: 直接通过访问alterTable子句别名访问
+        logger.info("visitAlterTable: {}", ctx.tableName.getText());
+        return visitChildren(ctx);
+    }
+
+    private Boolean hasAncestorByRuleIndex(ParserRuleContext ctx, int ancestorIdx) {
         ParserRuleContext current = ctx.getParent();  // Start with the parent
         while (current != null) {
             if (current.getRuleIndex() == ancestorIdx) {
+                return true;  // Found the ancestor
+            }
+            current = current.getParent();  // Move up to the next parent
+        }
+        return false;
+    }
+
+    private Boolean hasAncestorByClassName(ParserRuleContext ctx, String className){
+        ParserRuleContext current = ctx.getParent();  // Start with the parent
+        while (current != null) {
+            String[] parts = current.getClass().getName().split("\\.");
+            if (parts[parts.length-1].equals(className)) {
                 return true;  // Found the ancestor
             }
             current = current.getParent();  // Move up to the next parent
